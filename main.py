a559388d43743f8cc8357c1516ac20078e13a1ac
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-07 01:25:40 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from subtitle_parser import parse_srt_file
-from video_annotator import add_subtitles_to_video
-
-input_video_path = "E:\\Cliptogen\\output\\concatenated_video.mp4"
-srt_file_path = "E:\\Cliptogen\\output\\temp_1.srt"
-output_video_path = "E:\\Cliptogen\\done\\done.mp4"
-
-subtitles = parse_srt_file(srt_file_path)
-add_subtitles_to_video(input_video_path, subtitles, output_video_path)
