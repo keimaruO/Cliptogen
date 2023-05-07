a39a82f1a8ebf585a8a9b3ba899eb92ad98e106a
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-07 23:28:07 +0900

    Update video_annotator.py

--- video_annotator.py
+++ video_annotator.py
@@ -1,10 +1,27 @@
 import datetime
-from moviepy import editor
+from moviepy.editor import concatenate_videoclips, TextClip, CompositeVideoClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import textwrap
-import budoux
+from moviepy.editor import TextClip, CompositeVideoClip
+def add_subtitles_to_video(input_video_path, subtitles, output_video_path, font_path):
+    video = VideoFileClip(input_video_path)
+    fps = video.fps
+    clips = []
 
+    for subtitle in subtitles:
+        start_time = datetime.timedelta(hours=subtitle['start'].hour, minutes=subtitle['start'].minute, seconds=subtitle['start'].second, microseconds=subtitle['start'].microsecond).total_seconds()
+        end_time = datetime.timedelta(hours=subtitle['end'].hour, minutes=subtitle['end'].minute, seconds=subtitle['end'].second, microseconds=subtitle['end'].microsecond).total_seconds()
+        clip = video.subclip(start_time, end_time)
+        annotated_clip = annotate(clip, subtitle['text'], font_path)
+        clips.append(annotated_clip)
 
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile(output_video_path, fps=fps, codec='libx264', audio_codec='aac')
+
+    video.close()
+    for clip in clips:
+        clip.close()
+    final_clip.close()
 def wrap_text(text, max_length=16, max_lines=4):
     wrapped_lines = textwrap.wrap(text, width=max_length)
 
@@ -34,72 +51,39 @@ def sentence_parse_and_line_parse(text, max_line_length=15, max_lines=4):
 
     return parsed_text_list[:max_lines]
 
+font_path = 'C:/USERS/KEI11/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/NOTOSANSJP-BLACK.TTF'
 
 
-def budoux_parse_text(text, max_length=16):
-    budoux_parser = budoux.load_default_japanese_parser()
-    parsed_text_list = []
+def annotate(clip, txt, font_path, max_line_length=15, max_lines=4):
+    video_width, video_height = clip.size
+    fontsize = int(video_height * 0.08)
 
-    while len(text) > 0:
-        parsed_text = ""
-        overflow_text = ""
-
-        for budoux_parsed_text in budoux_parser.parse(text):
-            if max_length > len(parsed_text + budoux_parsed_text):
-                parsed_text += budoux_parsed_text
-            elif len(parsed_text) == 0 and len(budoux_parsed_text) > max_length:
-                parsed_text = budoux_parsed_text[0:max_length]
-                overflow_text += budoux_parsed_text[max_length:]
-            else:
-                overflow_text += budoux_parsed_text
-
-        parsed_text_list.append(parsed_text)
-        text = overflow_text
-
-    return parsed_text_list
-
-def annotate(clip, txt, max_line_length=15, max_lines=4):
     parsed_txt_list = sentence_parse_and_line_parse(txt, max_line_length, max_lines)
     parsed_txt = "\n".join(parsed_txt_list)
-    txtclip_white = editor.TextClip(parsed_txt,
-        fontsize=75,
-        font='C:/USERS/KEI11/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/NOTOSANSJP-BLACK.TTF',
+
+    txtclip_white = TextClip(parsed_txt,
+        fontsize=fontsize,
+        font=font_path,
         color='white'
     )
-    txtclip_black = editor.TextClip(parsed_txt,
-        fontsize=75,
-        font='C:/USERS/KEI11/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/NOTOSANSJP-BLACK.TTF',
+    txtclip_black = TextClip(parsed_txt,
+        fontsize=fontsize,
+        font=font_path,
         color='black',
         stroke_color="#000000",
         stroke_width=7
     )
-    txtclip_ffa3aa = editor.TextClip(parsed_txt,
-        fontsize=75,
-        font='C:/USERS/KEI11/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/NOTOSANSJP-BLACK.TTF',
+    txtclip_ffa3aa = TextClip(parsed_txt,
+        fontsize=fontsize,
+        font=font_path,
         color='#FFA3AA',
         stroke_color="#FFA3AA",
         stroke_width=12
     )
-    cvc = editor.CompositeVideoClip([
-        clip, 
+    cvc = CompositeVideoClip([
+        clip,
         txtclip_ffa3aa.set_position(('center', 'bottom')).set_layer(1),
         txtclip_black.set_position(('center', 'bottom')).set_layer(2),
         txtclip_white.set_position(('center', 'bottom')).set_layer(3)
     ])
     return cvc.set_duration(clip.duration)
-
-
-
-def add_subtitles_to_video(input_video_path, subtitles, output_video_path):
-    video = VideoFileClip(input_video_path)
-    clips = []
-
-    for subtitle in subtitles:
-        start_time = datetime.timedelta(hours=subtitle['start'].hour, minutes=subtitle['start'].minute, seconds=subtitle['start'].second, microseconds=subtitle['start'].microsecond).total_seconds()
-        end_time = datetime.timedelta(hours=subtitle['end'].hour, minutes=subtitle['end'].minute, seconds=subtitle['end'].second, microseconds=subtitle['end'].microsecond).total_seconds()
-        clip = video.subclip(start_time, end_time)
-        annotated_clip = annotate(clip, subtitle['text'])
-        clips.append(annotated_clip)
-
-    final_clip = editor.concatenate_videoclips(clips)
-    final_clip.write_videofile(output_video_path)
