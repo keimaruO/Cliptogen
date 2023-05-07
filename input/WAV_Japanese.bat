04d78101f16a44f3070ce2082cf8332439d54977
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-07 23:18:44 +0900

    Update WAV_Japanese.bat

--- input/WAV_Japanese.bat
+++ input/WAV_Japanese.bat
@@ -11,4 +11,4 @@ for /f "delims=" %%i in ('dir /b /a-d /o:d "%input_dir%\*.wav"') do (
     set /a counter+=1
 )
 
-whisper-ctranslate2 "%input_dir%\temp_1.wav" --model large-v2 --output_dir "%output_dir%" --compute_type auto --language Japanese --initial_prompt="金の玉 ホロ鯖 ほろさば 平和の像 キッスマシーン ホロライブ 5期生 ねぽらぼ あずきち そらちゃん ときのそら そら先輩 まつり先輩 シオン シオン先輩 しおんたん スバル先輩 大空スバル スバル 兎田ぺこら ぺこ ぺこーら ぺこちゃん ぺこら先輩 るしあ るーちゃん先輩 かなた先輩 天音かなた かなたん へい民 わため先輩 トワワ トワワ先輩 常闇トワ トワ様 ルーナ ルーナ先輩 雪花ラミィ 雪花さん ラミィ ワミィ 雪民 桃鈴ねね ねね ねねち ねっ子  ねっ子 獅白ぼたん ししろん こより こよちゃん クロエ いろは またねね こんねね カブトムシ"
+whisper-ctranslate2 "%input_dir%\temp_1.wav" --model large-v2 --output_dir "%output_dir%" --compute_type auto --language Japanese --initial_prompt="ホロ鯖 ほろさば ホロライブ 5期生 ねぽらぼ あずきち そらちゃん ときのそら そら先輩 まつり先輩 シオン シオン先輩 しおんたん スバル先輩 大空スバル スバル 兎田ぺこら ぺこ ぺこーら ぺこちゃん ぺこら先輩 るしあ るーちゃん先輩 かなた先輩 天音かなた かなたん へい民 わため先輩 トワワ トワワ先輩 常闇トワ トワ様 ルーナ ルーナ先輩 雪花ラミィ 雪花さん ラミィ ワミィ 雪民 桃鈴ねね ねね ねねち ねっ子  ねっ子 獅白ぼたん ししろん こより こよちゃん クロエ いろは またねね こんねね カブトムシ"
