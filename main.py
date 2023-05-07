d854b7b9f0c7aae77bb21a29d5d3f58eb5048cc6
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-07 23:34:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ srt_file_path = os.path.join(BASE_DIR, "output", "temp_1.srt")
 output_video_path = os.path.join(BASE_DIR, "done", "done.mp4")
 
 subtitles = parse_srt_file(srt_file_path)
-font_path = 'C:/USERS/KEI11/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/NOTOSANSJP-BLACK.TTF'
+font_path = os.path.join(BASE_DIR, "input", "NOTOSANSJP-BLACK.TTF")
 
 add_subtitles_to_video(input_video_path, subtitles, output_video_path, font_path)
