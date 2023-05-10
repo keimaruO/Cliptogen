c6d1b1846fe3b221e9974fa768bbc58dace488b4
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-10 11:09:51 +0900

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -1,6 +1,11 @@
 import os
 import subprocess
 import re
+import shutil
+
+def get_ffmpeg_path():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(script_dir, "ffmpeg.exe")
 
 def fix_special_characters(name):
     return re.sub(r'[^\x00-\x7F]+', '', name)
@@ -11,7 +16,8 @@ def concatenate_videos(input_dir, output_dir, output_name):
     with open("concat.txt", "w", encoding="utf-8") as f:
         for file in file_list:
             f.write(f"file '{os.path.join(input_dir, file)}'\n")
-    concat_command = f'ffmpeg -y -f concat -safe 0 -i concat.txt -c copy "{os.path.join(output_dir, output_name)}"'
+    ffmpeg_path = get_ffmpeg_path()
+    concat_command = f'{ffmpeg_path} -y -f concat -safe 0 -i concat.txt -c copy "{os.path.join(output_dir, output_name)}"'
     os.system(concat_command)
     os.remove("concat.txt")
 
@@ -19,10 +25,13 @@ def extract_and_concatenate_audio(input_dir, output_dir, output_name):
     file_list = [file for file in os.listdir(input_dir) if file.endswith('.mp4')]
     file_list = sorted(file_list, key=lambda x: int(re.search(r'\d+', x).group()))
     audio_files = []
+    ffmpeg_path = get_ffmpeg_path()
     for file in file_list:
         audio_output = os.path.join(output_dir, "{}.wav".format(re.search(r'\d+', file).group()))
         audio_files.append(audio_output)
         extract_command = f'ffmpeg -y -i "{os.path.join(input_dir, file)}" -vn -acodec pcm_s16le -ar 44100 -ac 2 "{audio_output}"'
+        subprocess.run(extract_command, shell=True, env=os.environ)
+        extract_command = f'{ffmpeg_path} -y -i "{os.path.join(input_dir, file)}" -vn -acodec pcm_s16le -ar 44100 -ac 2 "{audio_output}"'
         os.system(extract_command)
 
     with open("concat_audio.txt", "w", encoding="utf-8") as f:
@@ -30,6 +39,8 @@ def extract_and_concatenate_audio(input_dir, output_dir, output_name):
             f.write(f"file '{audio_file}'\n")
 
     concat_audio_command = f'ffmpeg -y -f concat -safe 0 -i concat_audio.txt -c copy "{os.path.join(output_dir, output_name)}"'
+    subprocess.run(concat_audio_command, shell=True, env=os.environ)
+    concat_audio_command = f'{ffmpeg_path} -y -f concat -safe 0 -i concat_audio.txt -c copy "{os.path.join(output_dir, output_name)}"'
     os.system(concat_audio_command)
     os.remove("concat_audio.txt")
     for audio_file in audio_files:
@@ -45,24 +56,18 @@ output_dir = os.path.join(BASE_DIR, "output")
 output_name = "concatenated_video.mp4"
 concatenate_videos(input_dir, output_dir, output_name)
 
-
 audio_output_dir = os.path.join(BASE_DIR, "output")
-audio_output_name = "concatenated_audio.wav"
+audio_output_name = "temp_1.wav"
 extract_and_concatenate_audio(input_dir, audio_output_dir, audio_output_name)
 
+input_wav = os.path.join(BASE_DIR, "output", "temp_1.wav")
+whisper_command = f'whisper-ctranslate2 "{input_wav}" --model large-v2 --compute_type auto --language Japanese'
+print(whisper_command)
+subprocess.run(whisper_command, shell=True, cwd=output_dir)
 
-subprocess.run([os.path.join(BASE_DIR, "input", "WAV_Japanese.bat")])
+output_srt_path = os.path.join(BASE_DIR, "output", "temp_1.srt")
+
 subprocess.run(["python", os.path.join(BASE_DIR, "main.py")])
 
-
-temp_1_wav_path = os.path.join(BASE_DIR, "output", "temp_1.wav")
-if os.path.exists(temp_1_wav_path):
-    os.remove(temp_1_wav_path)
-
-concat_txt_path = os.path.join(BASE_DIR, "input", "concat.txt")
-if os.path.exists(concat_txt_path):
-    os.remove(concat_txt_path)
-
-concat_audio_txt_path = os.path.join(BASE_DIR, "input", "concat_audio.txt")
-if os.path.exists(concat_audio_txt_path):
-    os.remove(concat_audio_txt_path)
+if os.path.exists(input_wav):
+    os.remove(input_wav)
