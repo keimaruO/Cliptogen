2c358d4d8d27c2abe84eb782d04af911a50e6097
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-15 17:12:07 +0900

    Update video_annotator.py

--- video_annotator.py
+++ video_annotator.py
@@ -2,8 +2,6 @@ import os
 import datetime
 from moviepy.editor import concatenate_videoclips, TextClip, CompositeVideoClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
-import textwrap
-from moviepy.editor import TextClip, CompositeVideoClip
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,13 +17,13 @@ def add_subtitles_to_video(input_video_path, subtitles, output_video_path, font_
     for index, subtitle in enumerate(subtitles):
         start_time = datetime.timedelta(hours=subtitle['start'].hour, minutes=subtitle['start'].minute, seconds=subtitle['start'].second, microseconds=subtitle['start'].microsecond).total_seconds()
         end_time = datetime.timedelta(hours=subtitle['end'].hour, minutes=subtitle['end'].minute, seconds=subtitle['end'].second, microseconds=subtitle['end'].microsecond).total_seconds()
-        
+
         if index == len(subtitles) - 1 and end_time > video_duration:
             end_time = video_duration
 
         clip = video.subclip(start_time, end_time)
         annotated_clip = annotate(clip, subtitle['text'], font_path)
-        clips.append(annotated_clip)
+        clips.append(annotated_clip.set_audio(clip.audio))
 
     final_clip = concatenate_videoclips(clips)
     final_clip.write_videofile(output_video_path, fps=fps, codec='libx264', audio_codec='aac')
@@ -35,40 +33,45 @@ def add_subtitles_to_video(input_video_path, subtitles, output_video_path, font_
         clip.close()
     final_clip.close()
 
-    
-def wrap_text(text, max_length=16, max_lines=4):
-    wrapped_lines = textwrap.wrap(text, width=max_length)
+def calculate_max_chars(video_width, fontsize):
+    # This is an estimation, you may need to adjust this value
+    chars_per_fontsize_unit = 1.5
+    return int(video_width // (fontsize / chars_per_fontsize_unit))
 
-    if len(wrapped_lines) > max_lines:
-        wrapped_lines = wrapped_lines[:max_lines - 1] + [''.join(wrapped_lines[max_lines - 1:])]
+def sentence_parse_and_line_parse(text, video_width, fontsize, max_lines=4, max_chars=21):
+    words = text.split(' ')
+    lines = []
+    line = ""
 
-    return wrapped_lines
+    for word in words:
+        # Check if adding the current word to the current line will exceed the maximum characters.
+        if len(line) + len(word) + 1 <= max_chars:
+            line += " " + word
+        else:
+            # If it exceeds the maximum characters, start a new line with the current word.
+            lines.append(line.strip())
+            line = word
 
-def sentence_parse_and_line_parse(text, max_line_length=15, max_lines=4):
-    space_parsed_text_list = text.split(' ')
-    parsed_text_list = []
+        # Break the loop when the maximum lines is reached.
+        if len(lines) == max_lines:
+            break
 
-    current_line = ""
-    for space_parsed_text in space_parsed_text_list:
-        wrapped_text_list = wrap_text(space_parsed_text, max_line_length)
-        for wrapped_text in wrapped_text_list:
-            if len(current_line) + len(wrapped_text) + 1 <= max_line_length:
-                current_line += " " + wrapped_text
-            else:
-                parsed_text_list.append(current_line.strip())
-                current_line = wrapped_text
-                if len(parsed_text_list) == max_lines - 1:
-                    break
-    if current_line:
-        parsed_text_list.append(current_line.strip())
-    return parsed_text_list[:max_lines]
+    # Add any remaining words to the last line.
+    if line and len(lines) < max_lines:
+        lines.append(line.strip())
 
-def annotate(clip, txt, font_path, max_line_length=15, max_lines=4):
+    # Adjust font size if any line exceeds the maximum characters
+    if any(len(line) > max_chars for line in lines):
+        fontsize = int(fontsize * (max_chars / max(len(line) for line in lines)))
+
+    return lines[:max_lines], fontsize
+
+
+
+def annotate(clip, txt, font_path, max_lines=4):
     video_width, video_height = clip.size
-    fontsize = int(video_height * 0.08)
-    textclip_size = (video_width, video_height)
-
-    parsed_txt_list = sentence_parse_and_line_parse(txt, max_line_length, max_lines)
+    fontsize = int(video_height * 0.088)
+    parsed_txt_list, fontsize = sentence_parse_and_line_parse(txt, video_width, fontsize, max_lines=max_lines, max_chars=21)
     parsed_txt = "\n".join(parsed_txt_list)
 
     txtclip_white = TextClip(parsed_txt,
@@ -95,5 +98,7 @@ def annotate(clip, txt, font_path, max_line_length=15, max_lines=4):
         txtclip_ffa3aa.set_position(('center', 'bottom')).set_layer(1),
         txtclip_black.set_position(('center', 'bottom')).set_layer(2),
         txtclip_white.set_position(('center', 'bottom')).set_layer(3)
-    ])
+    ], size=clip.size).set_duration(clip.duration)
+
     return cvc.set_duration(clip.duration)
+
