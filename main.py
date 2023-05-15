3040714077d67c3a58a383e200e90a5fbd02f276
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-15 17:11:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from subtitle_parser import parse_srt_file
 from video_annotator import add_subtitles_to_video
 
@@ -6,7 +7,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 input_video_path = os.path.join(BASE_DIR, "output", "concatenated_video.mp4")
 srt_file_path = os.path.join(BASE_DIR, "output", "temp_1.srt")
-output_video_path = os.path.join(BASE_DIR, "done", "done.mp4")
+current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+output_video_filename = f"done_{current_datetime}.mp4"
+output_video_path = os.path.join("G:/GoogleDrive/_Share/Share", output_video_filename)
 subtitles = parse_srt_file(srt_file_path)
 font_path = "Cliptogen/fonts/NOTOSANSJP-EXTRABOLD.TTF"
 add_subtitles_to_video(input_video_path, subtitles, output_video_path, font_path)
